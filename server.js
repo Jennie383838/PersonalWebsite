84d5963b88caf723cbf31985711f9b11632c08a8
Xavier Thong <xavierthong4@gmail.com>
2026-02-08 23:28:32 +0800

    updated

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/products', async (req, res) => {
 });
 
 /* C = Create product */
-app.post('/addproduct', async (req, res) => {
+app.post('/addproducts', async (req, res) => {
     const { card_name, card_price, card_status, card_image } = req.body;
     try {
         const connection = await mysql.createConnection(dbConfig);
@@ -45,7 +45,7 @@ app.post('/addproduct', async (req, res) => {
             'INSERT INTO defaultdb.Products (card_name, card_price, card_status, card_image) VALUES (?, ?, ?, ?)',
             [card_name, card_price, card_status, card_image]
         );
-        res.status(201).json({ message: 'Product created' });
+        res.status(201).json({ message: 'Products created' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Create error' });
@@ -53,7 +53,7 @@ app.post('/addproduct', async (req, res) => {
 });
 
 /* U = Update product */
-app.put('/updateproduct/:id', async (req, res) => {
+app.put('/updateproducts/:id', async (req, res) => {
     const { id } = req.params;
     const { card_name, card_price, card_status, card_image } = req.body;
     try {
@@ -62,7 +62,7 @@ app.put('/updateproduct/:id', async (req, res) => {
             'UPDATE defaultdb.Products SET card_name = ?, card_price = ?, card_status = ?, card_image = ? WHERE id = ?',
             [card_name, card_price, card_status, card_image, id]
         );
-        res.json({ message: 'Product updated' });
+        res.json({ message: 'Products updated' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Update error' });
@@ -70,7 +70,7 @@ app.put('/updateproduct/:id', async (req, res) => {
 });
 
 /* D = Delete product */
-app.delete('/deleteproduct/:id', async (req, res) => {
+app.delete('/deleteproducts/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const connection = await mysql.createConnection(dbConfig);
@@ -78,7 +78,7 @@ app.delete('/deleteproduct/:id', async (req, res) => {
             'DELETE FROM defaultdb.Products WHERE id = ?',
             [id]
         );
-        res.json({ message: 'Product deleted' });
+        res.json({ message: 'Products deleted' });
     } catch (err) {
         console.error(err);
 
