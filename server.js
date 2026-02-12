13fe22e45cac3e90a2998730c79a8c48cc487909
Xavier Thong <xavierthong4@gmail.com>
2026-02-12 14:34:36 +0800

    New vercel link

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const allowedOrigins = [
     "http://localhost:3000",
     "https://personalwebsite-1-ngee.onrender.com",
-    "https://ramis-dreamland-avrq.vercel.app/",
+    "https://ramis-dreamland-avrq.vercel.app",
 ];
 
 app.use(
